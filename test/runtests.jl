8c8d4a2bcc62deb0ad56215eaf5e83323bdc61c3
Fran Urbano <viraltux@gmail.com>
2021-10-02 13:16:33 +0200

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,33 @@ using ForecastData
 using Test
 
 @testset "ForecastData.jl" begin
-    # Write your tests here.
+
+    # co2
+    data = co2()
+    @test data isa DataFrame
+    @test size(data) == (4609,2)
+
+    data = co2(true)
+    @test data isa DataFrame
+    @test size(data) == (17166,17)
+
+    # london
+    data = london()
+    @test data isa DataFrame
+    @test size(data) == (132,15)
+
+    # quakes
+    data = quakes()
+    @test data isa DataFrame
+    @test size(data) == (71,2)
+
+    #seaborne
+    data = seaborne()
+    @test data isa DataFrame
+    @test size(data) == (2199,4)
+
+    data = seaborne(true)
+    @test data isa DataFrame
+    @test size(data) == (13209,10)
+    
 end
